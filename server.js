08a84ac7559fe3d426ea3b2d9e829934a668de7f
inkblotty <inkblot.and.blush@gmail.com>
2016-04-07 15:43:21 -0600

    removed public folder -- we don't need index.html; and removed old code referencing previous microservice from server.js

--- server.js
+++ server.js
@@ -17,44 +17,6 @@ app.get('/', function(req, res) {
 	res.json(responseJSON);
 })
 
-
-/*
-var moment = require('moment');
-
-app.get('/', function(req, res) {
-	res.sendFile(path.resolve('./public/index.html'));
-})
-
-app.get('/:date', function(req, res) {
-	var inputDate = req.params.date;
-
-	// if input is a unix date
-	if (inputDate.replace(/\d/g, "") === "") {
-		var naturalDate = moment(inputDate, "X").format('LL');
-		if (inputDate.length === 10) {
-			res.json(
-				{"unix": inputDate, "natural": naturalDate}
-			)
-		}
-		else {
-			res.json(
-				{"unix": null, "natural": null}
-			)
-		}
-	}
-
-	// if input is a natural date string
-	else if (moment(inputDate).isValid()) {	
-		var naturalDate = moment(inputDate, 'MMMM D, YYYY');
-		var unixDate = naturalDate.unix();
-		res.json(
-			{"unix": unixDate, "natural": naturalDate.format('LL')}
-		)
-	} 
-	else { res.json({"unix": null, "natural": null}) }
-	//else { res.sendFile(path.resolve('./public/index.html')); }
-})*/
-
 http.listen(port, function() {
 	console.log('listening on *:' + port);
 });
