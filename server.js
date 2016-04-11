489aeb23fc458fde3e218280b278534e68ba01a5
inkblotty <inkblot.and.blush@gmail.com>
2016-04-11 17:55:12 -0600

    removed unneeded comments

--- server.js
+++ server.js
@@ -3,31 +3,9 @@ var app = express();
 
 var http = require('http').Server(app);
 var path = require('path');
-//var requestIp= require('request-ip');
 var port = process.env.PORT || 8080;
 
-// ip is wrong ip at herokuapp link, but fine at localhost
-
-/*
-var clientIp = '';
-
-var setIp = function(req, res) {
-	clientIp = requestIp.getClientIp(req);
-	console.log(clientIp);
-};
-*/
-
-/*	
-getIP(function(err, ip) {
-	if (err) {
-		throw err
-	} else {
-		setIp = ip;
-	}
-})*/
-
 app.get('/', function(req, res) {
-
 	// clientIp will be ::1 on localhost, as it's a loopback
 	var clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
